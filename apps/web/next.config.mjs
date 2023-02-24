c4d89ed2792159057c55d1d35d26af8b6e9ee1f9
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-02-24 20:29:52 +0000

    feat: updates nextjs config

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,6 +1,17 @@
 /** @type {import('next').NextConfig} */
 const config = {
   reactStrictMode: true,
+  swcMinify: true,
+  i18n: {
+    locales: ['en'],
+    defaultLocale: 'en',
+  },
+  eslint: {
+    ignoreDuringBuilds: !!process.env.CI || true,
+  },
+  typescript: {
+    ignoreBuildErrors: !!process.env.CI || true,
+  },
 };
 
 export default config;
