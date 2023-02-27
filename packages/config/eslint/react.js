3b65e555ca3010ef946e4e9f9ea9f7420d7329d9
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-02-27 03:55:35 +0000

    feat: adds unit test eslint plugins

--- packages/config/eslint/react.js
+++ packages/config/eslint/react.js
@@ -6,6 +6,16 @@ module.exports = {
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
   ],
+  overrides: [
+    {
+      files: ['**/__tests__/**/*.[jt]s?(x)', '**/?(*.)+(spec|test).[jt]s?(x)'],
+      extends: [
+        'plugin:vitest/recommended',
+        'plugin:testing-library/react',
+        'plugin:jest-dom/recommended',
+      ],
+    },
+  ],
   globals: {
     React: 'writable',
   },
