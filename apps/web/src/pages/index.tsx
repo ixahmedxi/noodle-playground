aeb74359a12922779837364c2810e7ba9b6bd19d
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-02-27 04:41:28 +0000

    feat: adds type-check script

--- apps/web/src/pages/index.tsx
+++ apps/web/src/pages/index.tsx
@@ -4,8 +4,7 @@ import { NextPage } from 'next';
 const Home: NextPage = () => {
   return (
     <main>
-      <h1>Home page</h1>
-      <Typography />
+      <Typography variant="h1">Home page</Typography>
     </main>
   );
 };
