0f3fb10aea35c9bf5b7947cc80e51ad596bc9f60
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-03-01 15:47:53 +0000

    feat: adds playwright e2e to web app

--- apps/web/.eslintrc.cjs
+++ apps/web/.eslintrc.cjs
@@ -1,6 +1,12 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   extends: ['noodle/react', 'noodle/next'],
+  overrides: [
+    {
+      files: ['e2e/**/*.ts'],
+      extends: ['noodle/playwright'],
+    },
+  ],
   parserOptions: {
     babelOptions: {
       presets: [require.resolve('next/babel')],
