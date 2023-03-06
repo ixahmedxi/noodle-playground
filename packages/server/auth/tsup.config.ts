0c3b7355007cec0b43502e48568d6ed034e677a9
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-03-06 00:21:17 +0000

    feat(auth): adds react export

--- packages/server/auth/tsup.config.ts
+++ packages/server/auth/tsup.config.ts
@@ -1,7 +1,7 @@
 import { defineConfig } from 'tsup';
 
 export default defineConfig({
-  entryPoints: ['src/index.ts'],
+  entryPoints: ['src/index.ts', 'src/react.ts'],
   format: ['esm', 'cjs'],
   outDir: 'dist',
   dts: true,
