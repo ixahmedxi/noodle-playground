0077a3df146db4bc15d13f2b476b64512844b2a6
Ahmed Elsakaan <ahmedtarek3214@gmail.com>
2023-03-06 02:15:48 +0000

    fix: database package eslint configuration

--- packages/server/database/.eslintrc.cjs
+++ packages/server/database/.eslintrc.cjs
@@ -1,5 +1,9 @@
-/** @type {import('@eslint/eslintrc').Linter.Config} */
+/** @type {import('eslint').Linter.Config} */
 const config = {
+  parserOptions: {
+    tsconfigRootDir: __dirname,
+    project: ['./tsconfig.json'],
+  },
   // no extra config needed but here if in the future we need to add something.
 };
 
